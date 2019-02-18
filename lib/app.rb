bb0909679d4ae409c7670869e1a2083063b5aec2
James Ault <james.ault@johnlewis.co.uk>
2019-02-18 17:06:46 +0000

    Working named-cats form

--- lib/app.rb
+++ lib/app.rb
@@ -8,6 +8,14 @@ get '/secret' do
   "My name is james, and he is great"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name=["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name=params[:name]
+  @age=params[:age]
+  erb(:form)
 end
